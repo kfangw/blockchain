70e148820aaf113c5521131c1ca3871a3b41b0f3
kfangw <kfangw@gmail.com>
2019-06-20 11:21:42 +0900

    abci copied from tendermint

---
+++ abci/kvstoremain/kvstore.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kfangw/blockchain/abci/example/kvstore"
+	"github.com/kfangw/blockchain/abci/server"
+	"github.com/kfangw/blockchain/abci/types"
+	"github.com/kfangw/blockchain/libs/log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type logger interface {
+	Info(msg string, keyvals ...interface{})
+}
+
+func TrapSignal(logger logger, cb func()) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		for sig := range c {
+			logger.Info(fmt.Sprintf("captured %v, exiting...", sig))
+			if cb != nil {
+				cb()
+			}
+			os.Exit(0)
+		}
+	}()
+}
+
+func runService() error{
+	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+
+	// Create the application - in memory or persisted to disk
+	var app types.Application
+	app = kvstore.NewKVStoreApplication()
+
+	// Start the listener
+	srv, err := server.NewServer("tcp://0.0.0.0:26658", "socket", app)
+	if err != nil {
+		return err
+	}
+	srv.SetLogger(logger.With("module", "abci-server"))
+	if err := srv.Start(); err != nil {
+		return err
+	}
+
+	// Stop upon receiving SIGTERM or CTRL-C.
+	TrapSignal(logger, func() {
+		// Cleanup
+		srv.Stop()
+	})
+
+	// Run forever.
+	select {}
+}
+
+func main(){
+	runService()
+}
--- go.mod
+++ go.mod
@@ -3,17 +3,26 @@ module github.com/kfangw/blockchain
 go 1.12
 
 require (
+	github.com/btcsuite/btcd v0.0.0-20190115013929-ed77733ec07d
 	github.com/btcsuite/btcutil v0.0.0-20190425235716-9e5f4b9a998d
 	github.com/go-kit/kit v0.8.0
 	github.com/go-logfmt/logfmt v0.4.0
 	github.com/gogo/protobuf v1.2.1
-	github.com/golang/protobuf v1.3.1
+	github.com/golang/protobuf v1.3.2-0.20190517061210-b285ee9cfc6c
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf // indirect
 	github.com/jmhodges/levigo v1.0.0
+	github.com/magiconair/properties v1.8.0
 	github.com/pkg/errors v0.8.1
+	github.com/spf13/afero v1.2.1 // indirect
 	github.com/spf13/cobra v0.0.5
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/viper v1.4.0
 	github.com/stretchr/testify v1.3.0
 	github.com/syndtr/goleveldb v1.0.0
+	github.com/tendermint/go-amino v0.14.1
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/net v0.0.0-20190607181551-461777fb6f67
+	golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223 // indirect
 	google.golang.org/grpc v1.21.1
 )
